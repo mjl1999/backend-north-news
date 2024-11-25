876e3d55d48859a8ea0c50c0ac101327f71e135a
Matthew Lee <matthewjameslee@hotmail.co.uk>
2024-11-25 10:25:13 +0000

    added get appi endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,18 @@
 const endpointsJson = require("../endpoints.json");
+const request = require("supertest");
+const app = require("../app")
 /* Set up your test imports here */
+const db = require("../db/seeds/seed"); // the file we use seed to enter data into the database
+const data = require("../db/data/test-data"); // the initial test data to reseed our file with
+const dbConnection = require("../db/connection");
 
+afterAll(() => {
+  return dbConnection.end();
+});
+
+beforeEach(() => {
+    return db(data);
+});
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const {getApi} = require("./controllers/api.controller")
+
+app.get("/api", getApi)
+
+
+module.exports = app
